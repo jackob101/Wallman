4712c48cbb5ee772adb10c4dfa48dcca12af81a5
Jackob <pietrzyk.jakub001@gmail.com>
2021-10-10 20:40:19 +0200

    Small changes

--- .config/awesome/modules/autorun/init.lua
+++ .config/awesome/modules/autorun/init.lua
@@ -1,12 +1,19 @@
 local awful = require("awful")
 
 applications = {
-	"telegram-desktop",
-	"discord",
 	"picom --experimental-backends --config ~/.config/picom/config",
+}
+
+runOnce = {
+	"discord",
+	"telegram-desktop",
 	"flameshot",
 }
 
+for app = 1, #runOnce do
+	awful.spawn(runOnce[app], { urgent = false })
+end
+
 for app = 1, #applications do
-	awful.spawn.easy_async_with_shell(applications[app])
+	awful.spawn.easy_async_with_shell(applications[app], { urgent = false })
 end
