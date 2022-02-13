17fd50958e0de022aba6125e1de0c87caeea87f7
Jackob <pietrzyk.jakub001@gmail.com>
2022-02-13 18:19:11 +0100

    Cleaned aliases and changed rofi theme

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -1,11 +1,7 @@
 #! /bin/zsh
 
-alias configGit="/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME"
-alias configAdd="$HOME/skripts/commitAndPushConfig.sh"
 alias vim="/usr/bin/nvim"
-alias goalEdit="$HOME/skripts/modifyPersonalGoals.sh"
-alias config="$HOME/.config"
-alias wallpapers="$HOME/Wallpapers"
+alias walls="$HOME/Wallpapers"
 
 # Changing ls to exa
 alias ls='exa -laG  --group-directories-first'
