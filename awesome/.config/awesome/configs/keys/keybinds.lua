584fb202e1e7488b1e8b90f6a942c8f5012e38bf
Jackob <pietrzyk.jakub001@gmail.com>
2022-03-31 15:14:14 +0200

    Awesome: Added a nicer way to switch clients between screens

--- awesome/.config/awesome/configs/keys/keybinds.lua
+++ awesome/.config/awesome/configs/keys/keybinds.lua
@@ -4,6 +4,7 @@ local hotkeys_popup = require("awful.hotkeys_popup")
 local modkey = require("configs.keys.mod").modkey
 local volume_widget = require("widgets.volume-widget.volume")
 local input_helpers = require("configs.keys.input_helpers")
+local client_mover = require("modules.client_mover")
 
 require("awful.hotkeys_popup.keys")
 
@@ -492,47 +493,54 @@ keys.clientkeys = gears.table.join(
 	awful.key(
 		{ modkey },
 		"o",
-		function(c) c:move_to_screen() end,
-		{ description = "move to screen", group = "client", }
+		function (c)
+			client_mover(c)
+		end
 	),
-	awful.key(
-		{modkey, "Shift"},
-		"o",
-		function(c)
-			local currentIndex = c.screen.index
-			local nextIndex = ((currentIndex) % (screen.count())) + 1
-			c:move_to_screen(nextIndex)
-			local tags = screen[nextIndex].tags
-			for k, v in pairs(tags) do
-				local clientsLength = #(v:clients())
-				if clientsLength == 0 then
-					c:move_to_tag(screen[nextIndex].tags[k])
-					v:view_only()
-					break
-				end
-			end
-		end,
-		{ description = "Move to free tag on next screen and focus it", group = "client" }
-	),
-	awful.key(
-		{modkey, "Shift", "Ctrl"},
-		"o",
-		function(c)
-			local currentIndex = c.screen.index
-			local nextIndex = ((currentIndex) % (screen.count())) + 1
-			c:move_to_screen(nextIndex)
-			local tags = screen[nextIndex].tags
-			for k, v in pairs(tags) do
-				local clientsLength = #(v:clients())
-				if clientsLength == 0 then
-					c:move_to_tag(screen[nextIndex].tags[k])
-					c.urgent = true
-					break
-				end
-			end
-		end,
-		{ description = "Move to free tag on next screen without focusing", group = "client" }
-	),
+	-- awful.key(
+	-- 	{ modkey },
+	-- 	"o",
+	-- 	function(c) c:move_to_screen() end,
+	-- 	{ description = "move to screen", group = "client", }
+	-- ),
+	-- awful.key(
+	-- 	{modkey, "Shift"},
+	-- 	"o",
+	-- 	function(c)
+	-- 		local currentIndex = c.screen.index
+	-- 		local nextIndex = ((currentIndex) % (screen.count())) + 1
+	-- 		c:move_to_screen(nextIndex)
+	-- 		local tags = screen[nextIndex].tags
+	-- 		for k, v in pairs(tags) do
+	-- 			local clientsLength = #(v:clients())
+	-- 			if clientsLength == 0 then
+	-- 				c:move_to_tag(screen[nextIndex].tags[k])
+	-- 				v:view_only()
+	-- 				break
+	-- 			end
+	-- 		end
+	-- 	end,
+	-- 	{ description = "Move to free tag on next screen and focus it", group = "client" }
+	-- ),
+	-- awful.key(
+	-- 	{modkey, "Shift", "Ctrl"},
+	-- 	"o",
+	-- 	function(c)
+	-- 		local currentIndex = c.screen.index
+	-- 		local nextIndex = ((currentIndex) % (screen.count())) + 1
+	-- 		c:move_to_screen(nextIndex)
+	-- 		local tags = screen[nextIndex].tags
+	-- 		for k, v in pairs(tags) do
+	-- 			local clientsLength = #(v:clients())
+	-- 			if clientsLength == 0 then
+	-- 				c:move_to_tag(screen[nextIndex].tags[k])
+	-- 				c.urgent = true
+	-- 				break
+	-- 			end
+	-- 		end
+	-- 	end,
+	-- 	{ description = "Move to free tag on next screen without focusing", group = "client" }
+	-- ),
 	awful.key(
 		{ modkey },
 		"t",
