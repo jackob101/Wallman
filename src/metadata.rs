84462ac73d6a2ed3c812604fc7c2930184393c12
jackob <pietrzyk.jakub001@gmail.com>
2023-04-22 13:33:33 +0200

    Changed format for index file from csv to json. From few tests the speed difference is marginal and json is easier to edit/change

--- src/metadata.rs
+++ src/metadata.rs
@@ -3,20 +3,24 @@ use clap::builder::Str;
 use clap::parser::ValuesRef;
 use log::Metadata;
 use serde::{Deserialize, Serialize};
-use std::fs::DirEntry;
+use std::fs::{DirEntry, File};
 use std::path::{Iter, PathBuf};
 use std::{fs, io};
+use std::io::{BufReader, BufWriter, Write};
+
+
+pub const INDEX: &str = "index.json";
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct FileMetadata {
-    pub index: u32,
+    pub id: u32,
     #[serde(skip_serializing_if = "Vec::is_empty", default)]
     pub tags: Vec<String>,
 }
 
 impl FileMetadata {
     pub fn new(index: u32, tags: Vec<String>) -> FileMetadata {
-        FileMetadata { index, tags }
+        FileMetadata { id: index, tags }
     }
 
     pub fn add_tag(&mut self, new_tag: &String) -> Result<(), String> {
@@ -33,7 +37,7 @@ impl FileMetadata {
         match position_option {
             None => Err(format!(
                 "Tag {} is not assigned to ID {}",
-                tag_name, self.index
+                tag_name, self.id
             )),
             Some(value) => {
                 self.tags.remove(value);
@@ -44,7 +48,7 @@ impl FileMetadata {
 
     pub fn contains_tags(&self, tags: &Vec<String>) -> bool {
         for tag in tags {
-            if !self.tags.contains(&tag) {
+            if !self.tags.contains(tag) {
                 return false;
             }
         }
@@ -52,7 +56,7 @@ impl FileMetadata {
     }
 
     pub fn move_id(&mut self, to: u32) {
-        self.index = to;
+        self.id = to;
     }
 }
 
@@ -63,19 +67,15 @@ pub struct StorageMetadata {
 
 impl StorageMetadata {
     pub fn new(config: &EnvConfig) -> StorageMetadata {
-        let path_to_index_file = config.storage_directory.join("index.csv");
+        let path_to_index_file = config.storage_directory.join(INDEX);
 
-        let mut reader = csv::ReaderBuilder::new()
-            .has_headers(false)
-            .flexible(true)
-            .from_path(&path_to_index_file)
-            .expect("Couldn't open index.csv. Check if the file index.csv exists in storage directory. If it doesn't exists run 'tag init' or create it manually");
+        let reader = match File::open(&path_to_index_file){
+            Ok(reader) => reader,
+            Err(_) => todo!()
+        };
 
-        let metadata = reader
-            .deserialize()
-            .into_iter()
-            .map(|entry| entry.expect("Couldn't parse"))
-            .collect::<Vec<FileMetadata>>();
+        let metadata = serde_json::from_reader(BufReader::new(reader))
+            .expect("Failed to parse json");
 
         StorageMetadata {
             path: path_to_index_file,
@@ -97,7 +97,7 @@ impl StorageMetadata {
             return Err("Can't add tags to file that doesn't exists!".to_string());
         }
 
-        let metadata_for_index_option = self.metadata.iter_mut().find(|entry| entry.index.eq(&id));
+        let metadata_for_index_option = self.metadata.iter_mut().find(|entry| entry.id.eq(&id));
 
         match metadata_for_index_option {
             None => {
@@ -113,7 +113,7 @@ impl StorageMetadata {
     }
 
     pub fn remove_tag_from_id(&mut self, id: u32, tags: Vec<String>) -> Result<(), String> {
-        let metadata_for_index_option = self.metadata.iter_mut().find(|entry| entry.index.eq(&id));
+        let metadata_for_index_option = self.metadata.iter_mut().find(|entry| entry.id.eq(&id));
 
         match metadata_for_index_option {
             None => Err("File with specified ID doesn't exists".to_string()),
@@ -127,10 +127,10 @@ impl StorageMetadata {
     }
 
     pub fn remove_all_tags_from_id(&mut self, id: u32) -> Result<(), String> {
-        let index_in_vector = self.metadata.iter().position(|entry| entry.index == id);
+        let index_in_vector = self.metadata.iter().position(|entry| entry.id == id);
 
         match index_in_vector {
-            None => Err(format!("ID: {} not found in index.csv", id)),
+            None => Err(format!("ID: {} not found in {}", id, INDEX)),
             Some(value) => {
                 self.metadata.remove(value);
                 Ok(())
@@ -139,7 +139,7 @@ impl StorageMetadata {
     }
 
     pub fn move_index(&mut self, from: u32, to: u32) -> Result<(), String> {
-        let found_metadata_about_file = self.metadata.iter_mut().find(|entry| entry.index == from);
+        let found_metadata_about_file = self.metadata.iter_mut().find(|entry| entry.id == from);
 
         match found_metadata_about_file {
             None => Err("Index not found".to_string()),
@@ -167,24 +167,16 @@ impl StorageMetadata {
     }
 
     pub fn persist(&self) {
-        println!("persisting index.csv");
+        println!("persisting {}", INDEX);
 
-        let mut writer = csv::WriterBuilder::new()
-            .flexible(true)
-            .has_headers(false)
-            .from_path(&self.path)
-            .expect("Failed to create csv writer");
+        let file = fs::OpenOptions::new()
+            .write(true)
+            .create(true)
+            .truncate(true)
+            .open(&self.path)
+            .expect("Failed to open/create file");
+        serde_json::to_writer(&file, &self.metadata).expect("Failed to write");
 
-        for file_meta_data in self.metadata.iter() {
-            if file_meta_data.tags.is_empty() {
-                continue;
-            }
-            writer
-                .serialize(file_meta_data)
-                .expect("Couldn't serialize record");
-        }
-
-        writer.flush().expect("Couldn't flush writer");
     }
 
     fn name_with_id_predicate(index: u32, entry: io::Result<DirEntry>) -> bool {
