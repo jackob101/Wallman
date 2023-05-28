c3a1925610d5f6f4aee662869bb57d4e26b161e0
Jackob Pietrzyk <57868905+jackob101@users.noreply.github.com>
2023-05-28 13:31:10 +0200

    Wayland apps ( dunst, hyprland, eww, waybar ) (#3)

---
+++ hypr/.config/hypr/scripts/random_wallpaper.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+/bin/find $HOME/Wallpapers/ -maxdepth 1 -type f | /bin/grep -E "[0-9]" | /bin/shuf -n 1| xargs /bin/swww img --transition-type wipe --transition-angle 30 --transition-step 90
